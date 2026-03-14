187938df62c3efc46d9e3aa480979db45411e7a0
monaallam62 <monaallam5102@gmail.com>
2026-03-15 01:14:20 +0200

    Add project files.

---
+++ Assignment_OOP_6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_OOP_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
