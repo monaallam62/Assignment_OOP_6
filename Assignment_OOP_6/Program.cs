ec71dc18479431601ed716a10883b51dcbc7ed2c
monaallam62 <monaallam5102@gmail.com>
2026-03-15 01:46:27 +0200

    Question 1

--- Assignment_OOP_6/Program.cs
+++ Assignment_OOP_6/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 1
+            //Q1 : What is abstraction in OOP? How is it different from encapsulation? Give a real-world example (not from the session) that shows the difference between the two
+            //-- Abstraction is the process of exposing only what the user needs and hiding how it is implemented
+            //Abstraction:What an object does/Design level
+            //Encapsulation:How data is protected/IMplementation level
+            //-- Abstraction decides WHAT to show — Encapsulation decides HOW to protect what's hidden
+            //ATM Machine 
+            //abstracyion :When you withdraw money from an ATM, you only interact with options like Withdraw, Deposit, and Check Balance do not see the internal banking processes 
+            //encapsulation: Your bank account balance is stored securely inside the banking system, and you cannot modify it directly
+            #endregion
         }
     }
 }
