785ad1bf7de5e67af040049c7e1876a32687c01f
monaallam62 <monaallam5102@gmail.com>
2026-03-15 02:50:24 +0200

    Cinema Class

---
+++ Assignment_OOP_6/Classes/Cinema.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_OOP_6.Classes
+{
+    #region Cinema Class
+    public partial class Cinema
+    {
+        private Ticket[] tickets = new Ticket[20];
+        private Projector projector = new Projector();
+
+        public void OpenCinema()
+        {
+            Console.WriteLine("=== Cinema Opened ===");
+            projector.Start();
+        }
+
+        public void CloseCinema()
+        {
+            projector.Stop();
+            Console.WriteLine("=== Cinema Closed ===");
+        }
+
+        public void AddTicket(Ticket t)
+        {
+            for (int i = 0; i < tickets.Length; i++)
+            {
+                if (tickets[i] == null)
+                {
+                    tickets[i] = t;
+                    return;
+                }
+            }
+        }
+
+        public Ticket[] GetTickets()
+        {
+            return tickets;
+        }
+    }
+    #endregion
+}
