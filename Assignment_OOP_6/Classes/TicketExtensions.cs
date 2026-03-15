a23a6fccbc8d9102428e09f97d599c7928a5a935
monaallam62 <monaallam5102@gmail.com>
2026-03-15 02:48:47 +0200

    TicketExtensions

---
+++ Assignment_OOP_6/Classes/TicketExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_OOP_6.Classes
+{
+    #region TicketExtensions
+//    public static class TicketExtensions
+//    {
+//        public static string GenerateReceipt(this Ticket t)
+//        {
+//            return
+// $@"========== RECEIPT ==========
+//  Movie    : {t.MovieName}
+//  Type     : {t.GetType().Name}
+//  Price    : {t.Price}
+//  Final    : {t.CalculateFinalPrice():0.00}
+//  Status   : {(t.IsBooked() ? "Booked" : "Not Booked")}
+//=============================";
+//        }
+
+//        public static decimal TotalRevenue(this Ticket[] tickets)
+//        {
+//            decimal total = 0;
+
+//            foreach (var t in tickets)
+//            {
+//                if (t != null)
+//                    total += t.CalculateFinalPrice();
+//            }
+
+//            return total;
+//        }
+//    }
+    #endregion
+}
