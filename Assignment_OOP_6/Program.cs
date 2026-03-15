7562a15d838ef1f97c55b5b28149f215859acdc7
monaallam62 <monaallam5102@gmail.com>
2026-03-15 02:13:51 +0200

    Question 3

--- Assignment_OOP_6/Program.cs
+++ Assignment_OOP_6/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Assignment_OOP_6
+﻿using System;
+
+namespace Assignment_OOP_6
 {
     internal class Program
     {
@@ -21,6 +23,16 @@
             //2)Usage
             //Abstraction : You want to provide default behavior ,Classes share common implementation
             //Interfaces:Multiple unrelated classes need the same behavior,You want to define a capability or contract
+            #endregion
+            #region Question 3
+            //a) Can you write: Appliance a = new Appliance("LG"); ? Why or why not?
+            //Because Appliance is an abstract class, and abstract classes cannot be instantiated directly they are meant to be inherited by other classes
+            //b) What is the difference between the three methods: PowerConsumption(), Status(), and Label()? Why did the designer make each one abstract, virtual, or concrete?
+            //PowerConsumption(): abstract method , has no implementation and every derived class must override it ,Different appliances consume different amounts of power, so each class must define its own implementation
+            //Status(): virtual method, has a default implementation but can be overridden by derived classes, allows for a common status message while still giving flexibility for specific appliances to provide their own status if needed
+            //Label(): concrete method, has a complete implementation and cannot be overridden,Child classes inherit it as it is provides a standard way to label appliances with their brand name, ensuring consistency across all appliances
+            //c) If you call Status() on a Toaster object, what will it return? Why?
+            //Standby , because the Toaster class does not override the Status() method, so it uses the default implementation from the base class Appliance
             #endregion
         }
     }
