6281cfa3c6120c23b0cfeb91773472a4263baa70
monaallam62 <monaallam5102@gmail.com>
2026-03-15 02:33:03 +0200

    Enum

---
+++ Assignment_OOP_6/Enum/TypeOfTicket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_OOP_6.Enum
+{
+    #region Enum
+    //public enum TypeOfTicket
+    //{
+    //    A,
+    //    B,
+    //    C,
+    //    D,
+    //    E,
+    //    F
+    //}
+    #endregion
+}
