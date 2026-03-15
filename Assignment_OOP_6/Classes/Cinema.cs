9bd0b8f447b12238fc79edaf52ec8876c21955a3
monaallam62 <monaallam5102@gmail.com>
2026-03-15 02:51:18 +0200

    Cinema Class

--- Assignment_OOP_6/Classes/Cinema.cs
+++ Assignment_OOP_6/Classes/Cinema.cs
@@ -7,39 +7,39 @@ using System.Threading.Tasks;
 namespace Assignment_OOP_6.Classes
 {
     #region Cinema Class
-    public partial class Cinema
-    {
-        private Ticket[] tickets = new Ticket[20];
-        private Projector projector = new Projector();
+    //public partial class Cinema
+    //{
+    //    private Ticket[] tickets = new Ticket[20];
+    //    private Projector projector = new Projector();
 
-        public void OpenCinema()
-        {
-            Console.WriteLine("=== Cinema Opened ===");
-            projector.Start();
-        }
+    //    public void OpenCinema()
+    //    {
+    //        Console.WriteLine("=== Cinema Opened ===");
+    //        projector.Start();
+    //    }
 
-        public void CloseCinema()
-        {
-            projector.Stop();
-            Console.WriteLine("=== Cinema Closed ===");
-        }
+    //    public void CloseCinema()
+    //    {
+    //        projector.Stop();
+    //        Console.WriteLine("=== Cinema Closed ===");
+    //    }
 
-        public void AddTicket(Ticket t)
-        {
-            for (int i = 0; i < tickets.Length; i++)
-            {
-                if (tickets[i] == null)
-                {
-                    tickets[i] = t;
-                    return;
-                }
-            }
-        }
+    //    public void AddTicket(Ticket t)
+    //    {
+    //        for (int i = 0; i < tickets.Length; i++)
+    //        {
+    //            if (tickets[i] == null)
+    //            {
+    //                tickets[i] = t;
+    //                return;
+    //            }
+    //        }
+    //    }
 
-        public Ticket[] GetTickets()
-        {
-            return tickets;
-        }
-    }
+    //    public Ticket[] GetTickets()
+    //    {
+    //        return tickets;
+    //    }
+    //}
     #endregion
 }
