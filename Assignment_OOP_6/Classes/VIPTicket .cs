89e789a9d4366252728c6252755de183faddc416
monaallam62 <monaallam5102@gmail.com>
2026-03-15 02:53:00 +0200

    VIPTicket

---
+++ Assignment_OOP_6/Classes/VIPTicket .cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_OOP_6.Classes
+{
+    #region VIPTicket
+    //public class VIPTicket : Ticket
+    //{
+    //    public bool LoungeAccess { get; set; }
+
+    //    public decimal ServiceFee { get; set; } = 50;
+
+    //    public VIPTicket(string movie, decimal price, bool lounge)
+    //        : base(movie, price)
+    //    {
+    //        LoungeAccess = lounge;
+    //    }
+
+    //    public override decimal CalculateFinalPrice()
+    //    {
+    //        return (Price + ServiceFee) * 1.14m;
+    //    }
+
+    //    public override void Print()
+    //    {
+    //        Console.WriteLine($"[Ticket #{TicketId}] {MovieName} | VIP | Lounge: {(LoungeAccess ? "Yes" : "No")} | Fee: {ServiceFee} | Price: {Price} | Final: {CalculateFinalPrice():0.00} | Booked: {(isBooked ? "Yes" : "No")}");
+    //    }
+    //}
+    #endregion
+}
