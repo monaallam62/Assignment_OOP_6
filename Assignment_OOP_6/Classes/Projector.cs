f2dee03e22ec03a35370371c8061436578709a3a
monaallam62 <monaallam5102@gmail.com>
2026-03-15 02:51:58 +0200

    Projector

---
+++ Assignment_OOP_6/Classes/Projector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_OOP_6.Classes
+{
+    #region Projector
+    //public class Projector
+    //{
+    //    public void Start()
+    //    {
+    //        Console.WriteLine("Projector ON");
+    //    }
+
+    //    public void Stop()
+    //    {
+    //        Console.WriteLine("Projector OFF");
+    //    }
+    //}
+    #endregion
+}
