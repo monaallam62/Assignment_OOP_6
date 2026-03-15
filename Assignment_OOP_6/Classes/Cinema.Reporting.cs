acc4ab5a21b09b9dc7d1fa3b6d70a5ffa83d5fd2
monaallam62 <monaallam5102@gmail.com>
2026-03-15 02:49:45 +0200

    Partial Cinema Class

---
+++ Assignment_OOP_6/Classes/Cinema.Reporting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_OOP_6.Classes
+{
+    #region Partial Cinema Class
+    //public partial class Cinema
+    //{
+    //    public void PrintAllTickets()
+    //    {
+    //        Console.WriteLine("\n--- All Tickets (from Cinema.Reporting) ---");
+
+    //        foreach (var t in tickets)
+    //        {
+    //            if (t != null)
+    //                t.Print();
+    //        }
+    //    }
+    //}
+    #endregion
+}
